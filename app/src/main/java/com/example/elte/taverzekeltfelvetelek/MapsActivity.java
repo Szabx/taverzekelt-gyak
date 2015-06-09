73cac1f55438dd493c02764ddc817dce14ba294d
szabx <szabx90@gmail.com>
2015-06-10 01:59:12 +0300

    Current location + markers with custom description

--- app/src/main/java/com/example/elte/taverzekeltfelvetelek/MapsActivity.java
+++ app/src/main/java/com/example/elte/taverzekeltfelvetelek/MapsActivity.java
@@ -1,16 +1,26 @@
 package com.example.elte.taverzekeltfelvetelek;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity {
+public class MapsActivity extends FragmentActivity implements GoogleMap.OnMapLongClickListener, GoogleMap.OnMarkerClickListener {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
@@ -19,12 +29,62 @@ public class MapsActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
+
+        // Add map listener for long clicks
+        mMap.setOnMapLongClickListener(this);
+        mMap.setOnMarkerClickListener(this);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         setUpMapIfNeeded();
+    }
+
+    @Override
+    public void onMapLongClick(LatLng point) {
+
+        final LatLng newPoint = point;
+
+        // Init the dialog object
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Enter description");
+
+        // Set up the input
+        final EditText input = new EditText(this);
+
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String description;
+                description = input.getText().toString();
+
+                mMap.addMarker(new MarkerOptions()
+                        .position(newPoint)
+                        .title(description)
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+//        marker.setTitle("This is where the reason comes");
+//        marker.showInfoWindow();
+        return false;
     }
 
     /**
@@ -62,8 +122,32 @@ public class MapsActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        Marker kezdo = mMap.addMarker(new MarkerOptions().position(new LatLng(47.4911057, 19.0678168)).title("Marker").snippet("This is my spot!"));
-        Log.d("latitude", String.valueOf(kezdo.getPosition().latitude));
-        Log.d("longitude", String.valueOf(kezdo.getPosition().longitude));
+
+        mMap.setMyLocationEnabled(true);
+
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        Criteria criteria = new Criteria();
+
+        String provider = locationManager.getBestProvider(criteria, true);
+
+        Location location = locationManager.getLastKnownLocation(provider);
+
+
+        // Getting latitude of the current location
+        double latitude = location.getLatitude();
+
+        // Getting longitude of the current location
+        double longitude = location.getLongitude();
+
+
+        // Creating a LatLng object for the current location
+        LatLng latLng = new LatLng(latitude, longitude);
+
+        // Showing the current location in Google Map
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+        // Zoom in the Google Map
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
     }
 }
