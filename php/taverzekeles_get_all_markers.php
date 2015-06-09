7a94d611f4b68db5747090d3346ceed46754e06b
Zoli <nadasizoltan@hotmail.hu>
2015-06-09 23:41:58 +0200

    Az összes markert lekérdező php kód

---
+++ php/taverzekeles_get_all_markers.php
@@ -0,0 +1,38 @@
+<?php
+
+mysql_connect('mysql2.000webhost.com','a9524642_djzolee','szakdoga12') or die (mysql_error());
+mysql_select_db('a9524642_hive') or die (mysql_error());
+
+mysql_query('SET NAMES UTF8');
+mysql_query('SET CHARACTER SET UTF8');
+
+$result = mysql_query("SELECT *FROM markers") or die(mysql_error());
+
+$response = array();
+
+if (mysql_num_rows($result) > 0) {
+
+    $response["markers"] = array();
+    
+    while ($row = mysql_fetch_array($result)) {
+	
+        $product = array();
+		$product["id"] = $row["id"];
+        $product["lat"] = $row["lat"];
+        $product["lon"] = $row["lon"];
+		$product["date"] = $row["date"];
+		$product["megjegyzes"] = $row["megjegyzes"];
+
+
+        array_push($response["markers"], $product);
+    }
+    $response["success"] = 1;
+
+    echo json_encode($response);
+} else {
+    $response["success"] = 0;
+    $response["message"] = "No markers found";
+
+    echo json_encode($response);
+}
+?>
